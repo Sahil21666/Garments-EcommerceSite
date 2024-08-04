896c32f9c592c13ab8258d1c231797f7b301013f
Sahil Meshram <sahilmeshram222@gmail.com>
2024-08-04 18:39:51 +0530

    new changes

--- GarmentApi/api.js
+++ GarmentApi/api.js
@@ -4,6 +4,9 @@ const app = express();
 const port = 3000;
 const product_routes = require("./router/products")
 const conectDB = require("./connect")
+const cors = require("cors")
+
+app.use(cors());
 
 app.get('/',(req,res)=>{
     res.send("Hi, iam Live now")
